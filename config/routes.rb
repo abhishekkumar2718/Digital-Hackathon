3c56ffd23084fb1142895f6af9d58070fc752864
swaroop2000 <kkrishnaswaroop99@gmail.com>
2019-03-16 16:01:45 +0530

    Added NavBar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :elections
-  resources :candidates
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'event#index'
+  get 'event/index'
+  #root 'event#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
