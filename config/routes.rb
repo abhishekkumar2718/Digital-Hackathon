fb7fc37052edb9ccd1282f8806a87ef8c5144e3b
swaroop2000 <kkrishnaswaroop99@gmail.com>
2019-03-16 19:01:57 +0530

    Adding Customer Page and DB changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'event#index'
-  get 'event/index'
-  #root 'event#index'
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :elections
+  resources :candidates
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
