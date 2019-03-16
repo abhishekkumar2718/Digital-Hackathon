429bc5020d46859c25fd64345256dfb93424c6e0
Abhishek Kumar <abhishekkumar8222@gmail.com>
2019-03-16 18:33:56 +0000

    Modified User Profile Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
   root 'event#index'
-  #get 'event/index'
-  #get 'sessions/new'
-  #get 'sessions/create'
-  #get 'sessions/destroy'
   resources :elections
   resources :candidates
-  resources :users
+  get "user/profile", to: 'user#show', as: :user_profile
+  post "user/profile", to: 'user#update'
+  get "register", to: "user#new", as: :register
+  post "register", to: "user#create"
   resources :sessions, only: [:new, :create, :destroy]
-  #get ‘signup’, to: ‘users#new’, as: ‘signup’
-  #get ‘login’, to: ‘sessions#new’, as: ‘login’
-  #get ‘logout’, to: ‘sessions#destroy’, as: ‘logout’
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-
 end
