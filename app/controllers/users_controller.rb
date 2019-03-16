8e8974d47b0f803ad83506ab146ee8833f8271db
harsh <agrawal.harsh14@gmail.com>
2019-03-16 18:05:52 +0530

    Added User Authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  delegate :email, to: :user
   # GET /users
   # GET /users.json
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-
-  has_one :user_detail
+  has_one :user_detail, dependent: :destroy
   has_one :candidate
   has_many :votes
+  delegate :email, to: :user_detail
+
+  after_initialize do
+    self.build_user_detail if user_detail.nil?
+  end
 end
